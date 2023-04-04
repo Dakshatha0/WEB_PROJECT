222986c4354a43671cfc2240acfe0d99af5808c4
Dakshatha0 <kasturidakshatha@gmail.com>
2023-04-04 13:39:43 +0530

    commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const signUpController = async (req, res) => {
     try {
         const {email,password} = req.body;
@@ -25,14 +26,43 @@ const signUpController = async (req, res) => {
     }
 };
 
-const logInController = async(req, res) => {
+const logInController = async (req, res) => {
     try {
-        res.send('from login');
-    } catch (error) {
+        const { email, password } = req.body;
+        if(!email || !password){
+            return res.status(400).send("All fields are required");
+        }
+
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).send("User not registered");
+        } 
+        const matched = await bcrypt.compare(password, user.password);
+        if(!matched) {
+            return res.status(403).send("Incorrect password");
+        }
+        const accessToken = generateAccessToken({_id:user._id, email: user.email});
+        return res.json({accessToken});
+    } 
+    catch (error) {
         
+    }
+};
+
+//internal functions
+const generateAccessToken = (data) => {
+    try{
+        const token =  jwt.sign(data, 'asfhjdfhvdjvdfgdfftydyt', {
+            expiresIn: '1'
+        });
+        console.log(token);
+        return token;
+        }
+    catch(error){
+        console.log(error);
     }
 }
 module.exports = {
     signUpController,
-    logInController
+    logInController,
 }
--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = async() => {
-    const mongoUri = 'mongodb+srv://kasturidakshatha:0w4dYgQwpHi3Pb5L@cluster0.av5prs4.mongodb.net/?retryWrites=true&w=majority';
+    const mongoUri = 'mongodb+srv://kasturidakshatha:fDjiqmEud9mL4tcU@cluster0.av5prs4.mongodb.net/?retryWrites=true&w=majority';
 
     try{
         const connect = await mongoose.connect(mongoUri, {
