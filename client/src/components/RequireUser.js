8c5e1f1bcd7e8e580370b7428fd078d82e6deaab
Dakshatha0 <kasturidakshatha@gmail.com>
2023-04-25 10:58:52 +0530

    commit

--- client/src/components/RequireUser.js
+++ client/src/components/RequireUser.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { getItem, KEY_ACCESS_TOKEN } from '../utils/localStorageManager'
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 function RequireUser() {
     const user = getItem(KEY_ACCESS_TOKEN);
   return (
-    user ? <Outlet /> : <navigate to='/login' />
+    user ? <Outlet /> : <Navigate to='/login' />
   )
 }
 
