c3607143030490d50277c000d1f690327a9496e0
Dakshatha0 <kasturidakshatha@gmail.com>
2023-05-02 11:12:20 +0530

    commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -54,7 +54,115 @@ const getPostsOfFollowing = async (req, res) => {
         return res.send(error(500, e.message));
     }
 }
+const getMyPosts = async (req, res) => {
+    try {
+        const curUserId = req._id;
+        const allUserPosts = Post.find({
+            owner: curUserId,
+        }).populate('likes');
+
+        return res.send(success(200, {allUserPosts}));
+    } catch (error) {
+        console.log(error);
+        return res.send(error(500, e.message));
+    }
+}
+const getUserPosts = async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        if(!userId) {
+            return res.send(error(400, 'userid is required'));
+        }
+
+        const allUserPosts = await Post.find({
+            owner: userId,
+        }).populate('likes');
+
+        return res.send(success(200, {allUserPosts}));
+    } catch (error) {
+        return res.send(error(500, e.message));
+    }
+}
+const deleteMyProfile = async (req, res) => {
+    try {
+        const curUserId = req._id;
+        const curUser = await User.findById(curUserId);
+
+        //delete all posts
+        await Post.deleteMany({
+            owner: curUserId,
+        });
+
+        //remove myself from followers' following
+        curUser.followers.forEach(async (followerId) => {
+            const follower = await User.findById(followerId);
+            const index = follower.followings.indexOf(curUserId);
+            follower.followings.splice(index, 1);
+            await follower.save();
+        });
+        //remove myself from my followings' followers
+        curUser.followings.forEach(async (followingId) => {
+            const following = await User.findById(followingId);
+            const index = following.followers.indexOf(curUserId);
+            followings.followers.splice(index, 1);
+            await following.save();
+        });
+        //remove myself from all likes
+        const allPosts = await Post.find();
+        allPosts.forEach(async (post) => {
+            const index = post.likes.indexOf(curUserId);
+            post.likes.splice(index, 1);
+            await post.save();
+        });
+        //delete user
+        await curUser.remove();
+
+        res.clearCookie('jwt', {
+            httpOnly: true,
+            secure: true
+        })
+
+        return res.send(success(200, 'User deleted'));
+    } 
+    catch (error) {
+        console.log(e);
+        return res.send(error(500, e.message));
+    }
+};
+const getMyInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req._id);
+        return res.send(success(200, {user}));
+    }
+    catch(e) {
+        return res.send(error(500, e.message));
+    }
+};
+
+const updateUserProfile = async (req, res) => {
+    try {
+        const {name, bio} = req.body;
+
+        const user = await User.findById(req._id);
+        if(name) {
+            user.name = name;
+        }
+        if(bio) {
+            user.bio = bio;
+        }
+        await user.save();
+        return res.send(success(200, {user}));
+    } catch (error) {
+        console.log('error put',e);
+        return res.send(error(500, e.message));
+    }
+};
     module.exports = {
         followOrUnfollowUserController,
-        getPostsOfFollowing
+        getPostsOfFollowing,
+        getMyPosts,
+        getUserPosts,
+        updateUserProfile,
+        getMyInfo,
+        deleteMyProfile
     }
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,11 @@ const requireUser = require('../middlewares/requireUser');
 const UserController = require('../controllers/userController');
 
 router.post('/follow', requireUser, UserController.followOrUnfollowUserController);
-router.get('/getpostsOfFollowing', requireUser, UserController.getPostsOfFollowing);
+router.get('/getFeedData', requireUser, UserController.getPostsOfFollowing);
+router.get('/getMyPosts', requireUser, UserController.getMyPosts);
+router.delete('/', requireUser, UserController.deleteMyProfile);
+router.get('/getMyInfo', requireUser, UserController.getMyInfo);
+router.get('/getUserPosts', requireUser, UserController.getUserPosts);
 
+router.put('/', requireUser, UserController.updateUserProfile);
 module.exports = router;
