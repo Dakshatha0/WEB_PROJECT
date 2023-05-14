b264b9d1f06f6b681f9fbdf196040f04a35dbb1b
Dakshatha0 <kasturidakshatha@gmail.com>
2023-05-14 15:45:27 +0530

    commit

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import Avatar from '../avatar/Avatar'
 import './Post.scss'
-import backgroundImg from '../../assets/backgroundImage.jpg'
 import {AiOutlineHeart} from 'react-icons/ai'
+
 function Post({post}) {
   return (
     <div className='Post'>
         <div className="heading">
-            <Avatar/>
-            <h4>Dakshatha Kasturi</h4>
+            <Avatar src={post?.owner?.avatar?.url}/>
+            <h4>{post.owner?.name}</h4>
         </div>
         <div className="content">
-            <img src={backgroundImg} alt='' />
+            <img src={post.image.url} alt='' />
         </div>
         <div className="footer">
             <div className="like">
                 <AiOutlineHeart className='icon'/>
-                <h4>4 likes</h4>
+                <h4>{`${post.likesCount} likes`}</h4>
             </div>
-            <p className='caption'>This is a picture of building</p>
-            <h6 className='time-ago'>4 hours ago</h6>
+            <p className='caption'>{post.caption}</p>
+            <h6 className='time-ago'>{post.timeAgo}</h6>
         </div>
     </div>
   )
